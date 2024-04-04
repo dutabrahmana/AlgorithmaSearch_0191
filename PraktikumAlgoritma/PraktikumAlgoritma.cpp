204f08174f2fa64332315e36a7e884d71473609a
dutabrahmana <dutalinggau25@gmail.com>
2024-04-04 11:01:24 +0700

    membuat isi dari if tersebut

--- PraktikumAlgoritma/PraktikumAlgoritma.cpp
+++ PraktikumAlgoritma/PraktikumAlgoritma.cpp
@@ -55,7 +55,12 @@ void LinearSearch()
 			i++; // step 4
 		}
 
-		if 
+		if (i == n) // step 5
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
 
 	}
 }
